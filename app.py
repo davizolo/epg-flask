e477174ec8a43eb2a79761b8b18ea8d27a595ab4
davizolo <122052210+davizolo@users.noreply.github.com>
2025-09-09 15:24:42 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app = Flask(__name__, static_folder='static', static_url_path='/static')
 # Lista de canales oficiales que se mostrarán en la guía
 CHANNELS_OFICIALES = [
     "La 1 HD", "La 2", "Antena 3 HD", "Cuatro HD", "Telecinco HD", "La Sexta HD", "TVG Europa HD", "Teledeporte",
-    "M+ LaLiga HD", "M. LaLiga 2", "DAZN LaLiga", "DAZN LaLiga 2 HD", "#Vamos por M+", "Movistar Plus+", "DAZN 1", "DAZN 2", "LaLiga TV Hypermotion HD", "LaLiga TV Hypermotion 2", "Primera Federación", "M+ Ellas Vamos HD", "DAZN Liga F", "basket", 
+    "M+ LaLiga HD", "M. LaLiga 2", "DAZN LaLiga", "DAZN LaLiga 2 HD", "#Vamos por M+", "Movistar Plus+", "DAZN 1", "DAZN 2", "LaLiga TV Hypermotion HD", "LaLiga TV Hypermotion 2", "Primera Federación", "M+ Ellas Vamos HD", "DAZN Liga F", "basket", "parrulo", 
     "DAZN F1", "Eurosport 1", "Eurosport 2", "M. Deportes", "M. Deportes 2", "Liga de Campeones",
     "Liga de Campeones 2", "Liga de Campeones 3", "Liga de Campeones 4"
 ]
@@ -44,7 +44,7 @@ ALIAS_CANAL = {
     "M+ Vamos 1080": "#Vamos por M+", "M+ Ellas Vamos HD": "M+ Ellas Vamos", "M+ Ellas Vamos HD": "M+ Ellas Vamos HD", "DAZN Liga F": "DAZN Liga F", "Movistar Plus": "Movistar Plus+", "Movistar+": "Movistar Plus+",
     "Movistar Plus+ HD": "Movistar Plus+", "Dazn 1": "DAZN 1", "DAZN 1 HD": "DAZN 1", "Dazn 2": "DAZN 2",
     "DAZN 2 HD": "DAZN 2", "Hypermotion": "LaLiga TV Hypermotion HD", "LaLiga Hypermotion": "LaLiga TV Hypermotion HD",
-    "LaLiga TV Hypermotion": "LaLiga TV Hypermotion HD", "LaLiga TV Hypermotion 2": "LaLiga TV Hypermotion 2", "Primera Federación": "Primera Federación", "basket": "basket", 
+    "LaLiga TV Hypermotion": "LaLiga TV Hypermotion HD", "LaLiga TV Hypermotion 2": "LaLiga TV Hypermotion 2", "Primera Federación": "Primera Federación", "basket": "basket", "parrulo": "parrulo", 
     "Dazn F1": "DAZN F1", "DAZN F1 HD": "DAZN F1", "Eurosport1": "Eurosport 1",
     "Eurosport 1 HD": "Eurosport 1", "Eurosport2": "Eurosport 2", "Eurosport 2 HD": "Eurosport 2",
     "Movistar Deportes": "M. Deportes", "M Deportes": "M. Deportes", "M+ Deportes HD": "M. Deportes",
@@ -73,6 +73,7 @@ CANAL_TO_PNG = {
     "LaLiga TV Hypermotion 2": "hypermotion2.png",
     "Primera Federación": "1reff.png",
     "basket": "basket.png",
+    "parrulo": "parrulo.png", 
     "DAZN F1": "f1.png",
     "Eurosport 1": "eurosport.png",
     "Eurosport 2": "eurosport2.png",
